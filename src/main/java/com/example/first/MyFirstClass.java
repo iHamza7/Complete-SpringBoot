71ab6ed85be91dfd6e34f0b828628465c153fb62
iHamza7 <khanazam3324@gmail.com>
2024-06-23 23:48:26 +0500

    changed

--- src/main/java/com/example/first/MyFirstClass.java
+++ src/main/java/com/example/first/MyFirstClass.java
@@ -9,6 +9,6 @@ public class MyFirstClass {
     }
 
     public String sayHello() {
-        return "hello from my First Class 1 2 3 4 5 7 8 9 10 ===> MyVar =====> " + myVar;
+        return "hello from my First Class  ===> MyVar =====> " + myVar;
     }
 }
