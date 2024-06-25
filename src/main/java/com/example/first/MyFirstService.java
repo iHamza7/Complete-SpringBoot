a3cc8beabd67012352a4ac08e439e6e245845385
iHamza7 <khanazam3324@gmail.com>
2024-06-25 23:25:03 +0500

    changed

--- src/main/java/com/example/first/MyFirstService.java
+++ src/main/java/com/example/first/MyFirstService.java
@@ -19,7 +19,7 @@ public class MyFirstService {
     private int customPropertyInt;
     @Value("${my.custom.property}")
     private String customString;
-    @Value("${my.custom.property.int 1 1 2}")
+    @Value("${my.custom.property.int 1 1 2 4}")
     private int customInteger;
 
     public MyFirstService(
