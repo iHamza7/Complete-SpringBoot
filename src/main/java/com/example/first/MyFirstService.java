f0d4d95e1c4a707fcfbc8c9e5bc7d8cf23b609fb
iHamza7 <khanazam3324@gmail.com>
2024-06-26 23:52:30 +0500

    changed

--- src/main/java/com/example/first/MyFirstService.java
+++ src/main/java/com/example/first/MyFirstService.java
@@ -23,7 +23,7 @@ public class MyFirstService {
     private int customInteger;
 
     public MyFirstService(
-            @Qualifier("mySecondClass 1 1 q r g") MyFirstClass myFirstClass) {
+            @Qualifier("mySecondClass 1 1 q r g y") MyFirstClass myFirstClass) {
         this.myFirstClass = myFirstClass;
     }
 
