103b42e0bbeb9eb237ac848690d4fdf04fc7ca15
iHamza7 <khanazam3324@gmail.com>
2024-06-30 23:30:57 +0500

    changed

--- src/main/java/com/example/first/ApplicationConfig.java
+++ src/main/java/com/example/first/ApplicationConfig.java
@@ -14,6 +14,6 @@ public class ApplicationConfig {
     @Bean
 
     public MyFirstClass mySecondClass() {
-        return new MyFirstClass("Second Bean 1qq qq gg");
+        return new MyFirstClass("Second Bean 1qq qq gg ww");
     }
 }
